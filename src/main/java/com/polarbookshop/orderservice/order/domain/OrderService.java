8a45342ba45f82fa59f1146d6ba14ed36b21d477
dhami <dhamija.sandeep.cse@gmail.com>
2025-08-30 15:07:43 -0400

    added tests for WebClient using Mock server

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -1,5 +1,7 @@
 package com.polarbookshop.orderservice.order.domain;
 
+import com.polarbookshop.orderservice.book.Book;
+import com.polarbookshop.orderservice.book.BookClient;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -7,8 +9,10 @@ import reactor.core.publisher.Mono;
 @Service
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final BookClient bookClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
+        this.bookClient = bookClient;
         this.orderRepository = orderRepository;
     }
 
@@ -22,8 +26,17 @@ public class OrderService {
         return orderRepository.findAll();
     }
 
+    public static Order buildAcceptedOrder(Book book, int quantity) {
+        return Order.of(book.isbn(), book.title() + " - " + book.author(),
+                book.price(), quantity, OrderStatus.ACCEPTED);
+    }
+
     public Mono<Order> submitOrder(String isbn, int quantity) {
-        return Mono.just(buildRejectedOrder(isbn, quantity))
+        return bookClient.getBookByIsbn(isbn)
+      .map(book -> buildAcceptedOrder(book, quantity))
+      .defaultIfEmpty(
+                buildRejectedOrder(isbn, quantity)
+        )
                 .flatMap(orderRepository::save);
     }
 }
